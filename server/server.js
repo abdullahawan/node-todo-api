4015daa6a4000dd60b281fa0a5abf85c313c6508
Muhammad Awan <ayzad01@gmail.com>
2018-12-23 22:09:21 -0500

    Added POST/todos route and refactored mongoose

---
+++ server/server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    console.log(JSON.stringify(doc, undefined, 3));
+    res.send(doc);
+  }, (e) => {
+    console.log('Unsuccessful save to database', e)
+    res.status(400).send(e);
+  });
+});
+
+
+app.listen(3000, () => {
+  console.log('Started on Port 3000');
+});
