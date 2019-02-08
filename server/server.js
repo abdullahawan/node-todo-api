c0dcde9bcdf8dd48639123837ae579c2f2a3440a
Muhammad Awan <ayzad01@gmail.com>
2019-02-08 18:55:05 -0500

    Added remove capabilities. Tested DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.status(200).send();
+  res.redirect('/todos');
 });
 
 app.post('/todos', (req, res) => {
@@ -55,8 +55,29 @@ app.get('/todos/:id', (req, res) => {
   })
 });
 
+app.delete('/todos/:id', (req, res) => {
+  id = req.params.id;
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove({_id: id})
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.status(200).send({todo});
+    }, (e) => {
+      res.status(400).send();
+    });
+})
+
 app.listen(port, () => {
   console.log(`Started on Port ${port}`);
 });
+
+
 
 module.exports = {app};
